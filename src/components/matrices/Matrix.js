c76ea6727bbc3aefff5b0fce880b82a671f30f5b
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-04-22 17:42:51 +0500

    Added testing support

--- src/components/matrices/Matrix.js
+++ src/components/matrices/Matrix.js
@@ -1,12 +1,8 @@
 class Matrix extends Array {
   constructor(width, height) {
-    Array.prototype.push.apply(this, Matrix.create(width, height));
-  }
-
-  static create(w, h) {
-    const matrix = [];
-    while (h--) matrix.push(new Array(w).fill(0));
+    super();
+    while (height--) this.push(new Array(width).fill(0));
   }
 }
 
-export default Matrix;
+module.exports = Matrix;
--- src/static/elements.js
+++ src/static/elements.js
@@ -1,8 +1,11 @@
 const elements = {
   score: document.getElementById('score'),
-  canvas: document.getElementById('canvas'),
+  stopwatch: document.getElementById('stopwatch'),
   overlay: document.getElementById('overlay'),
-  records: document.getElementById('records'),
+  pause: document.getElementById('pause'),
+  controls: document.getElementById('controls'),
+  game: document.getElementById('game'),
+  preview: document.getElementById('preview'),
 };
 
 export default elements;
