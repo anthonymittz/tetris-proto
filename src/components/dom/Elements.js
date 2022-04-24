eff9bb536d006d731d8bcc158ccc91468305eb73
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-04-24 15:29:08 +0500

    Added Elements class

---
+++ src/components/dom/Elements.js
@@ -0,0 +1,13 @@
+class Elements {
+  constructor() {
+    this.score = document.getElementById('score');
+    this.stopwatch = document.getElementById('stopwatch');
+    this.overlay = document.getElementById('overlay');
+    this.pause = document.getElementById('pause');
+    this.controls = document.getElementById('controls');
+    this.game = document.getElementById('game');
+    this.preview = document.getElementById('preview');
+  }
+}
+
+export default Elements;
