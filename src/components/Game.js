41835f69b90ffc0ce9d39b711bfa76433f03f495
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-04-25 15:13:32 +0500

    Added class instanciation to main()

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,16 +1,11 @@
+const Player = require('./player/Player');
+
 class Game {
-  constructor() {
+  constructor(player) {
     this.score = 0;
-    this.tetrominos = {
-      current: 1,
-      next: 5,
-    };
-    this.player = {
-      pos: { x: 0, y: 0 },
-      rot: 0,
-    };
+    this.player = new Player();
     this.isPaused = true;
   }
 }
 
-export default Game;
+module.exports = Game;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
-import colors from './static/colors.json';
-import elements from './static/elements';
+// general
+const Game = require('./components/Game');
+const Storage = require('./components/Storage');
+const Controls = require('./components/Controls');
+const Events = require('./components/Events');
+// drawing
+const Canvas = require('./components/dom/Canvas');
+const Elements = require('./components/dom/Elements');
+const Overlay = require('./components/dom/Overlay');
+// mechanics
+const Player = require('./components/player/Player');
+// assets
+const colors = require('./static/colors.json');
 
-// window.addEventListener('keydown', e => {
-//   for (var type in controls) {
-//     controls[type].keys.forEach(key => e.code === key && controls[type].action());
-//   }
-// });
-// window.addEventListener('beforeunload', e => console.log('[unload]'));
+function main() {
+  const elements = new Elements();
+  const gameCanvas = new Canvas(elements.game, colors);
+  const previewCanvas = new Canvas(elements.preview, colors);
+
+  const game = new Game();
+}
+
+main();
