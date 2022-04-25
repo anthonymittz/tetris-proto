8e219b3483a4f84c07ea5406d34bf11f9f9a7bb8
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-04-25 19:05:45 +0500

    Fixed event handlers: call methods of a reference

--- src/components/main/events/Events.js
+++ src/components/main/events/Events.js
@@ -5,21 +5,19 @@ class Events {
 
   register() {
     window.addEventListener('keydown', e => this.keyDown(e.code));
-    window.addEventListener('beforeunload', () => this.beforeUnload);
+    window.addEventListener('beforeunload', () => this.beforeUnload());
   }
 
   unregister() {
-    window.removeEventListener('keydown', this.keyDown);
-    window.addEventListener('beforeunload', this.beforeUnload);
+    window.removeEventListener('keydown', e => this.keyDown(e.code));
+    window.addEventListener('beforeunload', () => this.beforeUnload());
   }
 
   keyDown(code) {
     this.controls.press(code);
   }
 
-  beforeUnload() {
-    console.log('[Events] unload');
-  }
+  beforeUnload() {}
 }
 
 module.exports = Events;
