43f6571ea87812f44fcc1e22f1c491ea950414bc
RoscandDesign <59227392+Roscand@users.noreply.github.com>
2022-04-25 18:25:42 +0500

    Added class instanciation to the main()

--- src/index.js
+++ src/index.js
@@ -1,23 +1,32 @@
 // general
-const Game = require('./components/Game');
-const Storage = require('./components/Storage');
-const Controls = require('./components/Controls');
-const Events = require('./components/Events');
+const Tetris = require('./components/Tetris');
+const Storage = require('./components/main/storage/Storage');
+const Controls = require('./components/main/controls/Controls');
+const Events = require('./components/main/events/Events');
 // drawing
 const Canvas = require('./components/dom/canvas/Canvas');
 const Elements = require('./components/dom/Elements');
 const Overlay = require('./components/dom/Overlay');
-// mechanics
-const Player = require('./components/player/Player');
 // assets
 const colors = require('./static/colors.json');
 
 function main() {
   const elements = new Elements();
-  const gameCanvas = new Canvas(elements.game, { colors });
-  const previewCanvas = new Canvas(elements.preview, { colors });
 
-  const game = new Game();
+  const tetrisProps = {
+    canvas: {
+      game: new Canvas(elements.game, { colors }),
+      preview: new Canvas(elements.preview, { colors }),
+    },
+    overlay: new Overlay(elements.overlay),
+  };
+
+  const tetris = new Tetris(tetrisProps);
+  const controls = new Controls(tetris);
+  const events = new Events(controls);
+
+  events.register();
+  tetris.run();
 }
 
 main();
